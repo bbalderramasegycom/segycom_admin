e605dfc993928b6d98a725b7f0858e1c59215c74
Bryan Balderrama <bryan.balderrama@segycom.com.mx>
2021-12-09 11:43:09 -0700

    Menu Items User Finished

--- src/components/ui/header/MenuItems.tsx
+++ src/components/ui/header/MenuItems.tsx
@@ -1,4 +1,9 @@
-import { Divider, Menu, MenuItem } from '@mui/material'
+import { Divider, ListItemIcon, Menu, MenuItem, Avatar  } from '@mui/material';
+import Logout from '@mui/icons-material/Logout';
+import { useDispatch } from 'react-redux';
+import { logout } from '../../state/actions/authAction';
+import { menuItemsStyles } from '../../../styles/ui/header/menuItemsStyles';
+
 
 interface Props {
     anchorEl: HTMLElement | null,
@@ -7,59 +12,41 @@ interface Props {
 }
 
 export const MenuItems = ({anchorEl, open, close}: Props) => {
+
+    const dispatch = useDispatch();
+
+    const styles = menuItemsStyles;
+
+    const handleLogout = () => {
+        dispatch( logout() );
+    }
+
     return (
         <Menu
             anchorEl={anchorEl}
             open={open}
             onClose={close}
             onClick={close}
-            // PaperProps={{
-            // elevation: 0,
-            // sx: {
-            //     overflow: 'visible',
-            //     filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-            //     mt: 1.5,
-            //     '& .MuiAvatar-root': {
-            //     width: 32,
-            //     height: 32,
-            //     ml: -0.5,
-            //     mr: 1,
-            //     },
-            //     '&:before': {
-            //     content: '""',
-            //     display: 'block',
-            //     position: 'absolute',
-            //     top: 0,
-            //     right: 14,
-            //     width: 10,
-            //     height: 10,
-            //     bgcolor: 'background.paper',
-            //     transform: 'translateY(-50%) rotate(45deg)',
-            //     zIndex: 0,
-            //     },
-            // },
-            // }}
-            // transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-            // anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+            PaperProps={{
+            elevation: 0,
+            sx: styles.menuItem,
+            }}
+            transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+            anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
         >
             <MenuItem>
-             Profile
+                <Avatar sx={{ bgcolor: "#424242" }}>B</Avatar> Cuenta
             </MenuItem>
-            <MenuItem>
+            {/* <MenuItem>
              My account
-            </MenuItem>
+            </MenuItem> */}
             <Divider />
-            <MenuItem>
             
-                Add another account
-            </MenuItem>
-            <MenuItem>
-            
-            Settings
-            </MenuItem>
-            <MenuItem>
-            
-            Logout
+            <MenuItem onClick={ handleLogout }>
+                <ListItemIcon>
+                    <Logout fontSize="small" />
+                </ListItemIcon>
+                Cerrar sesión
             </MenuItem>
         </Menu>
     )
