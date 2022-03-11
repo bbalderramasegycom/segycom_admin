42adb962d9d9dd70e070c292e107671a4203f139
Bryan Balderrama <bryan.balderrama@segycom.com.mx>
2022-03-11 12:54:36 -0700

    Dashboard merge fixed

--- src/components/routes/DashboardRoutes.tsx
+++ src/components/routes/DashboardRoutes.tsx
@@ -1,8 +1,11 @@
 import { Route, Routes } from "react-router"
 import { DashboardViwe } from '../views/DashboardViwe';
 import { Navbar } from '../ui/header/Navbar';
+<<<<<<< HEAD
 import { MenuDrawer } from '../ui/header/MenuDrawer';
 import { Box, Toolbar } from "@mui/material";
+=======
+>>>>>>> 09d9aa3 (Dachboard components created)
 import { Stock } from "../views/Stock";
 
 export const DashboardRoutes = () => {
